f7e0afb665d196b60fbca4601d4e2acd1701be75
Jesus Miguel Rodriguez Ortega <rooj_civ@hotmail.com>
2026-01-07 13:10:48 -0600

    ya mero

--- validar_viga.php
+++ validar_viga.php
@@ -121,16 +121,42 @@ $edad = $reporte['edad'] ?? 'SIN edad';
 $edad_muestreo = $reporte['edad'] ?? 'SIN edad';
 $revenimiento_dis = $reporte['revenimientoDis'] ?? 'SIN revenimientop';
 $revenimiento_r1 = $reporte['revenimientoR1'] ?? 'SIN revenimiento1';
+$revenimiento_r2 = $reporte['revenimientoR2'] ?? 'SIN revenimiento2';
 $tma = $reporte['tma'] ?? 'SIN tma';
 $aditivo = $reporte['aditivo'] ?? 'SIN aditivo';
+$olla = $reporte['olla'] ?? 'SIN olla';
 $carretilla = $reporte['carretilla'] ?? 'SIN carretilla';
 $cono = $reporte['cono'] ?? 'SIN cono';
 $cucharon = $reporte['cucharon'] ?? 'SIN cucharon';
+$enrasador = $reporte['enrasador'] ?? 'SIN enrasador';
+$flexometro = $reporte['flexometro'] ?? 'SIN flexometro';
+$termometro = $reporte['termometro'] ?? 'SIN termometro';
+$varilla = $reporte['varilla'] ?? 'SIN varilla';
+$mazo = $reporte['mazo'] ?? 'SIN mazo';
+$placa = $reporte['placa'] ?? 'SIN placa';
+$proporciones = $reporte['proporciones'] ?? 'SIN proporciones';
+$estado_molde1 = $reporte['estadoMolde1'] ?? 'SIN estado molde1';
+$estado_molde2 = $reporte['estadoMolde2'] ?? 'SIN estado molde2';
+$estado_molde3 = $reporte['estadoMolde3'] ?? 'SIN estado molde3';
+$id_especimen1 = $reporte['idEspecimen1'] ?? 'SIN id especimen1';
+$id_especimen2 = $reporte['idEspecimen2'] ?? 'SIN id especimen2';
+$id_especimen3 = $reporte['idEspecimen3'] ?? 'SIN id especimen3';
+$molde1 = $reporte['molde1'] ?? 'SIN molde 1';
+$molde2 = $reporte['molde2'] ?? 'SIN molde 2';
+$molde3 = $reporte['molde3'] ?? 'SIN molde 3';
 $concretera = $reporte['concretera'] ?? 'SIN concretera';
 $temperatura = $reporte['temperatura'] ?? 'SIN temperatura';
 $remision = $reporte['remision'] ?? 'SIN remision';
+$tipo_muestreo = $reporte['tipoMuestreo'] ?? 'SIN tipo_muestreo';
+$tipo_resistencia = $reporte['tipoResistencia'] ?? 'SIN tipo_resistencia';
+$validado = 0 ?? 'SIN validado';
 $volumen = $reporte['volumenMuestra'] ?? 'SIN volumen muestra';
+$volumen_total = $reporte['volumenTotal'] ?? 'SIN volumen total';
+$numero_reporte = $reporte['numeroReporte'] ?? 'SIN numero reporte';
+$muestra = $reporte['muestra'] ?? 'SIN muestra';
 $hora_muestreo = $reporte['horaMuestreo'] ?? 'SIN hora muestreo';
+$hora_llegada = $reporte['horaLLegada'] ?? 'SIN hora llegada';
+$hora_salida = $reporte['horaSalida'] ?? 'SIN hora salida';
 $hora_desmoldeo = "";
 $muestreo = $reporte['personal'] ?? 'SIN personal';
 $recibio = "";
@@ -174,90 +200,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['item'])) {
 	echo "<script>
 	alert('CLIENTE: ' + " . $elemento . ");
 </script>";
-	try {
-
-		// =========================
-		// 1️⃣ INSERT REPORTE (MASTER)
-		// =========================
-		$sqlInsertReporte = "
-		INSERT INTO registros_vigas_campo_actualizado (
-			id_reporte_concreto, obra, fecha, aditivo, carretilla, cliente, concretera,
-			cono, cucharon, edad, elemento_colado, enrasador,
-			estado_molde1, estado_molde2, estado_molde3, estado_molde4,
-			exp, fc, flexometro, hora_llegada, hora_muestreo, hora_salida,
-			id_especimen1, id_especimen2, id_especimen3, id_especimen4,
-			localizacion, mazo,
-			molde1, molde2, molde3, molde4,
-			muestra, numero_reporte, observaciones, personal, placa,
-			proporciones, remision,
-			revenimiento_dis, revenimiento_r1, revenimiento_r2,
-			revisado, temperatura, termometro, tipo_muestreo,
-			tipo_resistencia, tma, ubicacion, validado, varilla,
-			volumen_total, volumen_muestra, olla
-		) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?
-		)";
-
-
-
-		$stmt = $conexion->prepare($sqlInsertReporte);
-
-		$stmt->bind_param(
-			"isssssssssssssssssssssssssssssssssssssssssssssssssssss",
-			$_POST['id_reporte_concreto'],
-			$_POST['obra'],
-			$_POST['fecha'],
-			$aditivo,
-			$carretilla,
-			$_POST['cliente'],
-			$_POST['concretera'],
-			$cono,
-			$cucharon,
-			$_POST['edad'],//10
-			$_POST['elemento_colado'],
-			$_POST['enrasador'],
-			$_POST['estado_molde1'],
-			$_POST['estado_molde2'],
-			$_POST['estado_molde3'],
-			$_POST['estado_molde4'],
-			$_POST['exp'],
-			$_POST['fc'],
-			$_POST['flexometro'],
-			$_POST['hora_llegada'],//20
-			$_POST['hora_muestreo'],
-			$_POST['hora_salida'],
-			$_POST['id_especimen1'],
-			$_POST['id_especimen2'],
-			$_POST['id_especimen3'],
-			$_POST['id_especimen4'],
-			$_POST['localizacion'],
-			$_POST['mazo'],
-			$_POST['molde1'],
-			$_POST['molde2'],//30
-			$_POST['molde3'],
-			$_POST['molde4'],
-			$_POST['muestra'],
-			$_POST['numero_reporte'],
-			$_POST['observaciones'],
-			$_POST['personal'],
-			$_POST['placa'],
-			$_POST['proporciones'],
-			$_POST['remision'],
-			$_POST['revenimiento_dis'],//40
-			$_POST['revenimiento_r1'],
-			$_POST['revenimiento_r2'],
-			$_POST['revisado'],
-			$_POST['temperatura'],
-			$_POST['termometro'],
-			$_POST['tipo_muestreo'],
-			$_POST['tipo_resistencia'],
-			$_POST['tma'],
-			$_POST['ubicacion'],
-			$_POST['validado'],//50
-			$_POST['varilla'],
-			$_POST['volumen_total'],
-			$_POST['volumen_muestra'],
-			$_POST['olla']
-		);
 
 		$item   = [null, null, null];
 		$edad   = [null, null, null];
@@ -277,6 +219,93 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['item'])) {
 
 			$i++;
 		}
+
+	try {
+
+		// =========================
+		// 1️⃣ INSERT REPORTE (MASTER)
+		// =========================
+		$sqlInsertReporte = "
+		INSERT INTO registros_vigas_campo_actualizado (
+			id_reporte_concreto, obra, fecha, aditivo, carretilla, cliente, concretera,
+			cono, cucharon, edad, elemento_colado, enrasador,
+			estado_molde1, estado_molde2, estado_molde3, estado_molde4,
+			exp, fc, flexometro, hora_llegada, hora_muestreo, hora_salida,
+			id_especimen1, id_especimen2, id_especimen3, id_especimen4,
+			localizacion, mazo,
+			molde1, molde2, molde3, molde4,
+			muestra, numero_reporte, observaciones, personal, placa,
+			proporciones, remision,
+			revenimiento_dis, revenimiento_r1, revenimiento_r2,
+			revisado, temperatura, termometro, tipo_muestreo,
+			tipo_resistencia, tma, ubicacion, validado, varilla,
+			volumen_total, volumen_muestra, olla,llave
+		) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?
+		)";
+
+
+
+		$stmt = $conexion->prepare($sqlInsertReporte);
+
+		$stmt->bind_param(
+			"issssssssssssssssssssssssssssssssssssssssssssssssssssss",
+			$_POST['id_reporte_concreto'],
+			$_POST['obra'],
+			$_POST['fecha'],
+			$aditivo,
+			$carretilla,
+			$_POST['cliente'],
+			$_POST['concretera'],
+			$cono,
+			$cucharon,
+			$_POST['edad_muestreo'],//10
+			$_POST['elemento_colado'],
+			$enrasador,
+			$estado_molde1,
+			$estado_molde2,
+			$estado_molde3,
+			$_POST['estado_molde4'],
+			$expediente,
+			$_POST['fc'],
+			$flexometro,
+			$hora_llegada,//20
+			$_POST['hora_muestreo'],
+			$hora_salida,
+			$item[0],
+			$item[1],
+			$item[2],
+			$_POST['id_especimen4'],
+			$_POST['localizacion'],
+			$mazo,
+			$molde1,
+			$molde2,//30
+			$molde3,
+			$_POST['molde4'],
+			$muestra,
+			$_POST['reporte'],
+			$_POST['observacion'],
+			$muestreo,
+			$placa,
+			$proporciones,
+			$_POST['remision'],
+			$_POST['revenimiento_dis'],//40
+			$_POST['revenimiento_r1'],
+			$revenimiento_r2,
+			$_POST['revisado'],
+			$_POST['temperatura'],
+			$termometro,
+			$tipo_muestreo,
+			$tipo_resistencia,
+			$_POST['tma'],
+			$_POST['ubicacion'],
+			$validado,//50
+			$varilla,
+			$volumen_total,
+			$volumen,
+			$olla,
+			$llave
+		);
+
 
 
 		if (!$stmt->execute()) {
