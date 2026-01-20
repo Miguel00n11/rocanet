5bcb22f4639bc4e45a35df5827ea88ac7af57961
Miguel00n11 <90936808+Miguel00n11@users.noreply.github.com>
2026-01-20 10:46:04 -0600

    fa

--- eliminar_reporte.php
+++ eliminar_reporte.php
@@ -12,13 +12,13 @@ $conexion->begin_transaction();
 
 try {
 
-    // // 1️⃣ Eliminar registros hijos
-    // $sql1 = "DELETE FROM registros_concreto_campo_actualizado 
-    //          WHERE id_reporte_concreto = ?";
-    // $stmt1 = $conexion->prepare($sql1);
-    // $stmt1->bind_param("i", $idReporte);
-    // $stmt1->execute();
-    // $stmt1->close();
+    // 1️⃣ Eliminar registros hijos
+    $sql1 = "DELETE FROM registros_concreto_campo_actualizado 
+             WHERE id_reporte_concreto = ?";
+    $stmt1 = $conexion->prepare($sql1);
+    $stmt1->bind_param("i", $idReporte);
+    $stmt1->execute();
+    $stmt1->close();
 
     // 2️⃣ Eliminar reporte principal
     $sql = "DELETE FROM reporte_concreto WHERE id_reporte_concreto = ?";
